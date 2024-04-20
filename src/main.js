e3956b1a7e7e2ee00c42d641a16a75dd3a96a64b
luq <luqman.yazrie@outlook.com>
2024-04-20 10:36:09 +0800

    organised and optimised

--- src/main.js
+++ src/main.js
@@ -1,45 +1,85 @@
 import kaboom from "kaboom"
 
 var btc = 0; var usd = 0
+let user_data = {
+	"gpu": [
+		{
+			"company": "Nvidia",
+			"name": "GeForce RTX 4090",
+			"hashrate": 156000,
+			"power": 450,
+			"heatscore": 80,
+			"lifespan": 220,
+			"price": 1099
+		},
+	],
+}
 
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const k = kaboom()
+class topBar{
+	// Mining group
 
-k.scene("mining", () => {
-	//Kaboom stuff
-	k.setBackground(0,0,0)
-
-	// Wallet category
-	const walletFrame = k.add([
+	// Wallet group
+	walletFrame = k.add([
 		k.rect(600,50),
 		k.pos(10, 10),
 		k.anchor("topleft"),
 		k.color(100,100,100)
 	]);
-
-	const btcText = k.add([
+	
+	btcText = k.add([
 		k.text(""),
 		k.pos(20,20),
 		k.anchor("topleft")
 	]);
-
-	const usdText = k.add([
+	
+	usdText = k.add([
 		k.text(""),
 		k.pos(300,20),
 		k.anchor("topleft")
 	]);
-	
-	// Mining script
+
+	// Exchange group
+}
+
+function execute() {
+	// Mining calculation script
 	(async () => {
 		while (true) {
-			btc += 0.1;
+				
+			btc += 0.05;
 			usd += 1.356;
-			btcText.text = `BTC: ${btc.toFixed(3)}`
-			usdText.text = `USD: ${usd.toFixed(3)}`
+			bar.btcText.text = `BTC: ${btc.toFixed(3)}`
+			bar.usdText.text = `USD: ${usd.toFixed(3)}`
+			console.log(`BTC: ${btc}`)
 			await delay(1000);
 		}
 	})();
+
+	// Energy calculation script
+	(async () => {
+		while (true) {
+			await delay(1000)
+		}
+	})();
+}
+
+k.scene("mining", () => { // Mining 
+	//Kaboom stuff
+	k.setBackground(0,0,0)
+
+	bar = new topBar();
+	
+	execute()
 });
+
+k.scene("wallet", () => {
+});
+
+k.scene("exchange", () => {});
+
+k.scene("shop", () => {});
 
 k.go("mining");
