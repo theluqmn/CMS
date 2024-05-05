77be2d0c732fd3b5827b88c9df203f5b14887482
luq <luqman.yazrie@outlook.com>
2024-05-06 03:25:50 +0800

    Fully working execute script

--- src/scripts/execute.js
+++ src/scripts/execute.js
@@ -8,8 +8,10 @@ export function Execute() {
         while (true) {
             cycle++;
 
-            // Reset values
-            game.mining.hashrate = 0;
+            // Reset values for each cycle
+            game.power.consumption = 0; game.power.production = 0;
+            game.mining.btc.hashrate = 0; game.mining.eth.hashrate = 0;
+
 
             // Loop each ownedGPU for mining
             for (let i = 0; i < ownedGPU.length; i++) {
@@ -17,18 +19,65 @@ export function Execute() {
                 referenceGPU = gpu[currentGPU.company][currentGPU.product][currentGPU.model];
                 coin = ownedGPU[i].crypto;
                 
-                game.power.consumption += referenceGPU.power * currentGPU.clock;
+                game.power.consumption += referenceGPU.powerConsumption * currentGPU.clock;
 
-                switch (coin) { // Coin-based
+                switch (coin) { // Mine according to coin
                     case "btc":
                         game.mining.btc.hashrate += referenceGPU.hashrate * currentGPU.clock;
+                        break;
                     case "eth":
-                        game.mining.eth.hashrate += referenceGPU.hashrate * currentGPU.clock;;
+                        game.mining.eth.hashrate += referenceGPU.hashrate * currentGPU.clock;
+                        break;
                 };
             };
+            
+            // Loop owned power generators
+            for (let i = 0; i < ownedPowerGenerator.length; i++) {
+                currentPowerGenerator = ownedPowerGenerator[i];
+                referencePowerGenerator = powerGenerator[currentPowerGenerator.type][currentPowerGenerator.model];
 
+                game.power.production += referencePowerGenerator.powerProduction;
+                wallet.usd -= currentPowerGenerator.upkeep; // Deduct wallet for power generation upkeep
+            };
+            
+            // Check if consuming more power than produced
+            if (game.power.production < game.power.consumption) {
+                console.log("Power consumption is higher than production")
+                game.power.grid = game.power.production - game.power.consumption;
+                wallet.usd += game.power.grid * game.power.gridPrice; // Deduct wallet by how much power is taken from grid
+                
+            } else {
+                console.log("Power production is higher than consumption")
+                game.power.grid = 0;
+            };
+
+            // Calculate mining rate
             game.mining.btc.miningRate = game.mining.btc.hashrate / game.mining.btc.difficulty;
             game.mining.eth.miningRate = game.mining.eth.hashrate / game.mining.eth.difficulty;
+
+            // Add to wallet
+            wallet.crypto.btc += game.mining.btc.miningRate;
+            wallet.crypto.eth += game.mining.eth.miningRate;
+
+            // Logging
+            game.mining.btc.log.push({ // BTC mining
+                cycle: cycle,
+                hashrate: game.mining.btc.hashrate,
+                miningRate: game.mining.btc.miningRate
+            });
+            game.mining.eth.log.push({ // ETH mining
+                cycle: cycle,
+                hashrate: game.mining.eth.hashrate,
+                miningRate: game.mining.eth.miningRate
+            });
+            game.power.log.push({ // Power
+                cycle: cycle,
+                consumption: game.power.consumption,
+                production: game.power.production,
+                grid: game.power.grid
+            });
+
+            console.log(`Cycle: ${cycle}\nBTC: ${game.mining.btc.miningRate} / ${wallet.crypto.btc}\nETH: ${game.mining.eth.miningRate} / ${wallet.crypto.eth}\nPower: ${game.power.production} / ${game.power.consumption}\nUSD: ${wallet.usd}\n`);
 
             // Each cycle happens every 1000ms (one second)
             await new Promise(r => setTimeout(r, 1000));
