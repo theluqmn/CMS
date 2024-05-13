ea1df7c68a9cbd40945a4b13a40d757e5b59507a
luq <luqman.yazrie@outlook.com>
2024-05-13 15:09:05 +0800

    Crypto section of wallet scene

--- src/ui/Wallet.js
+++ src/ui/Wallet.js
@@ -8,47 +8,74 @@ export function WalletScene() {
 
         // Title
         const Title = k.add([
-            k.text("Wallet Scene"),
+            k.text("Your Wallet"),
             k.pos(10, 150)
         ]);
 
-        // Frame
-        const CryptoSelect = k.add([
-            k.rect(400, 500),
+        // Select a coin Frame
+        const CoinFrame = k.add([
+            k.rect(600, 690),
             k.pos(10, 200),
             k.color(0, 0, 0),
             k.outline(3, k.rgb(255, 255, 255)),
         ]);
 
-        const CryptoHeading = k.add([
+        const CoinHeading = k.add([
             k.text("Select a coin"),
             k.scale(0.75),
             k.pos(20, 210),
         ]);
 
-        const CryptoBTC = k.add([
-            k.text("[BTC] Bitcoin"),
+        const CoinSelectFrame = k.add([
+            k.rect(580, 55),
             k.pos(20, 250),
+            k.color(0, 0, 0),
+            k.outline(3, k.rgb(200, 200, 200)),
+        ]);
+
+        const CoinBTC = k.add([
+            k.text("[BTC] Bitcoin"),
+            k.pos(25, 260),
             k.color(200, 200, 200),
             k.area()
         ]);
 
-        const CryptoETH = k.add([
+        const CoinETH = k.add([
             k.text("[ETH] Ethereum"),
-            k.pos(20, 300),
+            k.pos(300, 260),
             k.color(200, 200, 200),
             k.area()
         ]);
+
+        // Coin Stats
+        const CoinBalance = k.add([
+            k.text("Coin Balance"),
+            k.pos(20, 400),
+            k.color(255, 255, 255)
+        ]);
+
+        const CoinValue = k.add([
+            k.text("Coin Value"),
+            k.pos(20, 450),
+            k.color(255, 255, 255)
+        ]);
+
+        const totalOwned = k.add([
+            k.text("Total Owned"),
+            k.pos(20, 500),
+            k.color(255, 255, 255)
+        ]);
+
 
         // Buttons
-        CryptoBTC.onClick(() => {
+        CoinBTC.onClick(() => {
             selectedCrypto = "BTC";
-            console.log("Changed selected crypto BTC");
+            console.log("Changed selected coin to BTC");
         });
 
-        CryptoETH.onClick(() => {
+        CoinETH.onClick(() => {
             selectedCrypto = "ETH";
-            console.log("Changed selected crypto ETH");
+            console.log("Changed selected coin to ETH");
         });
 
         ChangeScenes();
@@ -57,14 +84,15 @@ export function WalletScene() {
 
             switch (selectedCrypto) {
                 case "BTC":
-                    CryptoBTC.color = k.rgb(255, 255, 255);
-                    CryptoETH.color = k.rgb(200, 200, 200);
+                    CoinBTC.color = k.rgb(255, 255, 255);
+                    CoinETH.color = k.rgb(200, 200, 200);
 
                     break;
+
                 case "ETH":
-                    CryptoETH.color = k.rgb(255, 255, 255);
-                    CryptoBTC.color = k.rgb(200, 200, 200);
-                    
+                    CoinETH.color = k.rgb(255, 255, 255);
+                    CoinBTC.color = k.rgb(200, 200, 200);
+
                     break;
             };
         });
