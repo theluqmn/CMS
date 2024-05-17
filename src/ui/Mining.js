c31eadaa1fe02c9d4aeb52e77e9410c04caab2c0
luq <luqman.yazrie@outlook.com>
2024-05-17 11:16:55 +0800

    values for crypto mining ops

--- src/ui/Mining.js
+++ src/ui/Mining.js
@@ -14,19 +14,20 @@ export function MiningScene() {
 
         // Mining Frame
         k.add([
-            k.rect(790, 400),
+            k.rect(790, 490),
             k.pos(10, 200),
             k.color(0, 0, 0),
             k.outline(3, k.rgb(255, 255, 255)),
         ]);
 
+        // Select coin
         const CoinHeading = k.add([
             k.text("Select a coin"),
             k.scale(0.75),
             k.pos(20, 210),
         ]);
 
-        const CoinSelectFrame = k.add([
+        const CoinSelectFrame = k.add([ // Selection Frame
             k.rect(580, 55),
             k.pos(20, 250),
             k.color(0, 0, 0),
@@ -45,6 +46,40 @@ export function MiningScene() {
             k.pos(300, 260),
             k.color(200, 200, 200),
             k.area()
+        ]);
+
+        // Actual stats
+        const hashrateHeading = k.add([ // Hashrate
+            k.text("Hashrate"),
+            k.scale(0.75),
+            k.pos(20, 320),
+        ]);
+
+        const hashrateValue = k.add([
+            k.text("10.000k"),
+            k.pos(20, 350),
+        ]);
+
+        const miningRateHeading = k.add([ // Mining Rate
+            k.text("Mining Rate"),
+            k.scale(0.75),
+            k.pos(20, 420),
+        ]);
+
+        const miningRateValue = k.add([
+            k.text("100k"),
+            k.pos(20, 450),
+        ]);
+
+        const usdRateHeading = k.add([ // USD Rate
+            k.text("Coin value per cycle:"),
+            k.scale(0.75),
+            k.pos(20, 520),
+        ]);
+
+        const usdRateValue = k.add([
+            k.text("100k"),
+            k.pos(20, 550),
         ]);
 
         // Trigger functions
