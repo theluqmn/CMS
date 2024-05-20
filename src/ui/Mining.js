e37b4878a10ff737cd12c879d870ef1425ee7c89
luq <luqman.yazrie@outlook.com>
2024-05-20 10:57:35 +0800

    power section addition, mining UI

--- src/ui/Mining.js
+++ src/ui/Mining.js
@@ -7,22 +7,20 @@ export function MiningScene() {
     k.scene("mining", () => {
         let selectedCrypto = "BTC";
 
-        // Mining Title
-        k.add([
-            k.text("Mining Scene"),
-            k.pos(20, 150)
+        // Mining stats
+        k.add([ // Title
+            k.text("Mining Stats"),
+            k.pos(10, 150)
         ]);
 
-        // Mining Frame
         k.add([
-            k.rect(790, 490),
+            k.rect(600, 490),
             k.pos(10, 200),
             k.color(0, 0, 0),
             k.outline(3, k.rgb(255, 255, 255)),
         ]);
 
-        // Select coin
-        const CoinHeading = k.add([
+        const CoinHeading = k.add([ // Select coin
             k.text("Select a coin"),
             k.scale(0.75),
             k.pos(20, 210),
@@ -50,48 +48,127 @@ export function MiningScene() {
         ]);
 
         // Actual stats
-        const hashrateHeading = k.add([ // Hashrate
+        const HashrateHeading = k.add([ // Hashrate
             k.text("Hashrate"),
             k.scale(0.75),
             k.pos(20, 320),
         ]);
 
-        const hashrateValue = k.add([
+        const HashrateValue = k.add([
             k.text("10.000k"),
             k.pos(20, 350),
         ]);
 
-        const miningRateHeading = k.add([ // Mining Rate
+        const MiningRateHeading = k.add([ // Mining Rate
             k.text("Mining Rate"),
             k.scale(0.75),
             k.pos(20, 420),
         ]);
 
-        const miningRateValue = k.add([
+        const MiningRateValue = k.add([
             k.text("100k"),
             k.pos(20, 450),
         ]);
 
-        const usdRateHeading = k.add([ // USD Rate
+        const UsdRateHeading = k.add([ // USD Rate
             k.text("Coin value per cycle:"),
             k.scale(0.75),
             k.pos(20, 520),
         ]);
 
-        const usdRateValue = k.add([
+        const UsdRateValue = k.add([
             k.text("100k"),
             k.pos(20, 550),
         ]);
 
-        const totalMinedHeading = k.add([ // Total Mined
+        const TotalMinedHeading = k.add([ // Total Mined
             k.text("Total Mined"),
             k.scale(0.75),
             k.pos(20, 620),
         ]);
 
-        const totalMinedValue = k.add([
+        const TotalMinedValue = k.add([
             k.text("100k"),
             k.pos(20, 650)
+        ]);
+
+        // Power stats
+        const PowerTitle = k.add([ // Title
+            k.text("Power Stats"),
+            k.pos(620, 150)
+        ]);
+
+        const PowerFrame = k.add([
+            k.rect(970, 490),
+            k.pos(620, 200),
+            k.color(0, 0, 0),
+            k.outline(3, k.rgb(255, 255, 255)),
+        ]);
+
+        const PowerProductionHeading = k.add([ // Power Production
+            k.text("Power Production"),
+            k.scale(0.75),
+            k.pos(640, 210),
+        ]);
+
+        const PowerProductionValue = k.add([
+            k.text("100k"),
+            k.pos(640, 240),
+        ]);
+
+        const PowerConsumptionHeading = k.add([ // Power Consumption
+            k.text("Power Consumption"),
+            k.scale(0.75),
+            k.pos(640, 310),
+        ]);
+
+        const PowerConsumptionValue = k.add([
+            k.text("100k"),
+            k.pos(640, 340),
+        ]);
+
+        const PowerUpkeepHeading = k.add([ // Power Upkeep
+            k.text("Power Upkeep"),
+            k.scale(0.75),
+            k.pos(640, 410),
+        ]);
+
+        const PowerUpkeepValue = k.add([
+            k.text("100k"),
+            k.pos(640, 440),
+        ]);
+
+        const GridConsumptionHeading = k.add([ // Grid Power Consumption
+            k.text("Power Grid Usage"),
+            k.scale(0.75),
+            k.pos(1240, 210),
+        ]);
+
+        const GridConsumptionValue = k.add([
+            k.text("100k"),
+            k.pos(1240, 240),
+        ]);
+
+        const GridPriceHeading = k.add([ // Grid Price
+            k.text("Grid Price"),
+            k.scale(0.75),
+            k.pos(1240, 310),
+        ]);
+
+        const GridPriceValue = k.add([
+            k.text("100k"),
+            k.pos(1240, 340),
+        ]);
+
+        const GridExpensesHeading = k.add([ // Grid Expenses (grid consumption * price)
+            k.text("Grid Expenses"),
+            k.scale(0.75),
+            k.pos(1240, 410),
+        ]);
+
+        const GridExpensesValue = k.add([
+            k.text("100k"),
+            k.pos(1240, 440),
         ]);
 
         // Trigger functions
@@ -113,23 +190,27 @@ export function MiningScene() {
             // Based on the selected crypto
             switch (selectedCrypto) {
                 case "BTC":
+                    // Highlight BTC
                     CoinBTC.color = k.rgb(255, 255, 255);
                     CoinETH.color = k.rgb(200, 200, 200);
 
-                    hashrateValue.text = NumRound(game.mining.btc.hashrate);
-                    miningRateValue.text = NumRound(game.mining.btc.miningRate);
-                    usdRateValue.text = `USD ${NumRound(game.mining.btc.miningRate * game.exchange.btc)}`;
-                    totalMinedValue.text = NumRound(game.mining.btc.totalMined);
+                    // Text set to BTC values
+                    HashrateValue.text = NumRound(game.mining.btc.hashrate);
+                    MiningRateValue.text = NumRound(game.mining.btc.miningRate);
+                    UsdRateValue.text = `USD ${NumRound(game.mining.btc.miningRate * game.exchange.btc)}`;
+                    TotalMinedValue.text = NumRound(game.mining.btc.totalMined);
                     break;
 
                 case "ETH":
+                    // Highlight ETH
                     CoinETH.color = k.rgb(255, 255, 255);
                     CoinBTC.color = k.rgb(200, 200, 200);
 
-                    hashrateValue.text = NumRound(game.mining.eth.hashrate);
-                    miningRateValue.text = NumRound(game.mining.eth.miningRate);
-                    usdRateValue.text = `USD ${NumRound(game.mining.eth.miningRate * game.exchange.eth)}`;
-                    totalMinedValue.text = NumRound(game.mining.eth.totalMined);
+                    // Text set to ETH values
+                    HashrateValue.text = NumRound(game.mining.eth.hashrate);
+                    MiningRateValue.text = NumRound(game.mining.eth.miningRate);
+                    UsdRateValue.text = `USD ${NumRound(game.mining.eth.miningRate * game.exchange.eth)}`;
+                    TotalMinedValue.text = NumRound(game.mining.eth.totalMined);
                     break;
             };
         });
